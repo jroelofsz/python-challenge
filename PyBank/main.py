4c1738143b3ebbcb38a7917390d765e7d439bd35
jroelofsz <jacobroelofsz@Jakes-Laptop.local>
2024-07-28 17:04:41 -0500

    Reworked my PyBank project to be correct, old version was producing incorrect values, also added image of results

--- PyBank/main.py
+++ PyBank/main.py
@@ -20,76 +20,70 @@ analysis_txt = os.path.join(".","analysis","budget_analysis.txt")
 
 
 #Declare Variables to be used throughout the script
-grand_total = 0
-average_change = 0
-total_change = 0
-change = 0
 month = []
-returns = []
+changes = []
+grand_total = 0
+previous_value = 0
+low_change = 0
+high_change = 0
+high_month = ["",0]
+low_month = ["",0]
+
 
 #Open the budget_data_csv file as source
 with open(budget_data_csv, 'r') as source:
 	csvreader = csv.reader(source,delimiter=',')
 	header = next(csvreader)
 
-	#Loop through each row in the CSV File
 	for row in csvreader:
-		#Add the profit/loss to the grand total
-		grand_total += int(row[1])
-
-		#Add each month and value to their own list, this is for calculating the min and max
+		current_value = int(row[1])
 		month.append(row[0])
-		returns.append(int(row[1]))
-
-
-
-#Find the index of the highest and lowest profit
-high_index = returns.index(max(returns))
-low_index = returns.index(min(returns))
+		grand_total += current_value
 		
+		if previous_value != 0:
+			change = current_value - previous_value
+			changes.append(change)
+
+			if change > high_change:
+				high_change = change
+				high_month[0] = row[0]
+				high_month[1] = change
+			elif change < low_change:
+				low_change = change
+				low_month[0] = row[0]
+				low_month[1] = change
+			
+
+		previous_value = current_value
 
 
-#Calculate the average change in profit/loss
-cur_val = 0
-prev_val = 0
-for row in returns:
-	cur_val = row
+	average_change = sum(changes) / len(changes)
 
-	#the change is current value - previous value)
-	change = cur_val - prev_val
-	prev_val = row
 
-	total_change += average_change + change
 
-#average change is the 'The changes in Profit/Losses over the entire period, 
-#and then the average of those changes'
-average_change = total_change / len(month)
 
 #results to command line
 print('Financial Analysis\n'
 					'-------------------------------\n'
-					f'Total Months: {len(month)}\n'
-					f'Total: ${grand_total}\n'
-					f'Average Change: ${round(average_change,2)}\n'
-					f'Greatest Increase in Profits: {month[high_index]} (${returns[high_index]})\n'
-					f'Greatest Decrease is Profits: {month[low_index]} (${returns[low_index]})\n'
-		)
-
-
+ 					f'Total Months: {len(month)}\n'
+ 					f'Total: ${grand_total}\n'
+ 					f'Average Change: ${round(average_change,2)}\n'
+ 					f'Greatest Increase in Profits: {high_month[0]} (${high_month[1]})\n'
+ 					f'Greatest Decrease is Profits: {low_month[0]} (${low_month[1]})\n'
+)
 
 
 #Write results to the file
 with open(analysis_txt, 'w', newline='') as output:
 	
-	#write results
-	output.write('Financial Analysis\n'
-					'-------------------------------\n'
-					f'Total Months: {len(month)}\n'
-					f'Total: ${grand_total}\n'
-					f'Average Change: ${round(average_change,2)}\n'
-					f'Greatest Increase in Profits: {month[high_index]} (${returns[high_index]})\n'
-					f'Greatest Decrease is Profits: {month[low_index]} (${returns[low_index]})\n'
-		)
+ 	#write results
+ 	output.write('Financial Analysis\n'
+ 					'-------------------------------\n'
+ 					f'Total Months: {len(month)}\n'
+ 					f'Total: ${grand_total}\n'
+ 					f'Average Change: ${round(average_change,2)}\n'
+ 					f'Greatest Increase in Profits: {high_month[0]} (${high_month[1]})\n'
+ 					f'Greatest Decrease is Profits: {low_month[0]} (${low_month[1]})\n')
 
 
 
