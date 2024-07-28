4ace342ad3cf66dcb2a1fedd78d387d33ce2b158
jroelofsz <jacobroelofsz@Jakes-Laptop.local>
2024-07-28 17:26:38 -0500

    some final polishing changess added, commented code throughout to ensure readability

--- PyBank/main.py
+++ PyBank/main.py
@@ -20,12 +20,9 @@ analysis_txt = os.path.join(".","analysis","budget_analysis.txt")
 
 
 #Declare Variables to be used throughout the script
-month = []
+months = 0
 changes = []
 grand_total = 0
-previous_value = 0
-low_change = 0
-high_change = 0
 high_month = ["",0]
 low_month = ["",0]
 
@@ -35,28 +32,42 @@ with open(budget_data_csv, 'r') as source:
 	csvreader = csv.reader(source,delimiter=',')
 	header = next(csvreader)
 
+	#variables to be used in loop
+	previous_value = 0
+	low_change = 0
+	high_change = 0
+
 	for row in csvreader:
+		#append the values to the
 		current_value = int(row[1])
-		month.append(row[0])
+		#month counter
+		months += 1
+		#add to the grand total
 		grand_total += current_value
 		
 		if previous_value != 0:
+			#change equal to the current value minus the previous value
 			change = current_value - previous_value
+			#add the change to the changes list (used for average)
 			changes.append(change)
 
+			#if change > largest change we've recorded, record the new one
 			if change > high_change:
 				high_change = change
 				high_month[0] = row[0]
 				high_month[1] = change
+			#if change < lowest change we've recorded, record the new one
 			elif change < low_change:
 				low_change = change
 				low_month[0] = row[0]
 				low_month[1] = change
 			
 
+		#set previous value = to current value for next run
 		previous_value = current_value
 
 
+	#calculate the average change
 	average_change = sum(changes) / len(changes)
 
 
@@ -65,7 +76,7 @@ with open(budget_data_csv, 'r') as source:
 #results to command line
 print('Financial Analysis\n'
 					'-------------------------------\n'
- 					f'Total Months: {len(month)}\n'
+ 					f'Total Months: {months}\n'
  					f'Total: ${grand_total}\n'
  					f'Average Change: ${round(average_change,2)}\n'
  					f'Greatest Increase in Profits: {high_month[0]} (${high_month[1]})\n'
@@ -79,7 +90,7 @@ with open(analysis_txt, 'w', newline='') as output:
  	#write results
  	output.write('Financial Analysis\n'
  					'-------------------------------\n'
- 					f'Total Months: {len(month)}\n'
+ 					f'Total Months: {months}\n'
  					f'Total: ${grand_total}\n'
  					f'Average Change: ${round(average_change,2)}\n'
  					f'Greatest Increase in Profits: {high_month[0]} (${high_month[1]})\n'
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,10 +13,12 @@ The script processes the dataset to extract and compute these values, providing 
 #Import Modules
 import os, csv
 
+#path variables
 election_data_csv = os.path.join(".","Resources","election_data.csv")
 analysis_txt = os.path.join(".","analysis","election_results.txt")
 
-total_votes = 0 
+#2 lists for tracking candidates and their vote count
+#each candidate will be assigned their own index. 
 candidates = []
 votes = []
 
@@ -28,45 +30,61 @@ with open(election_data_csv, 'r') as source:
 	
 	
 	for row in csvreader:
-		#add to the total votes
-		total_votes += 1
 	
 		if row[2] in candidates:
+			#if we've already collected the candidate, increment their vote count up 1
 			index = candidates.index(row[2])
 			votes[index] += 1
 		else:
 			#if this is the first time we are seeing this
-			#candidate, create a index
+			#candidate, create an index
 			index = len(candidates)
 			candidates.append(row[2])
 			votes.append(1)
 
-#find who the winner is
+#Find who the winner is
 winner_index = votes.index(max(votes))
 
-#return the results to comand line
-print('Election Results')
-print('--------------------')
-print(f'Total Votes: {total_votes}')
-print('--------------------')
+
+
+#------------------
+#Return the results to command line
+print(
+	'Election Results\n'
+	'--------------------\n'
+	f'Total Votes: {sum(votes)}\n'
+	'--------------------\n'
+)
+
+#This will dynamically output each candidate and their votes. 
+#This should also work if you provide more than 3 candidates in the same format
+#csv file
 for x in range(0,len(candidates)):
-	print(f'{candidates[x]}: {round(votes[x] / total_votes * 100,2)}% ({votes[x]})')
-print('--------------------')	
-print(f'Winner: {candidates[winner_index]}')
-print('--------------------')		
+	print(f'{candidates[x]}: {round(votes[x] / sum(votes) * 100,3)}% ({votes[x]})')
+
+print(
+	'\n--------------------\n'	
+	f'Winner: {candidates[winner_index]}\n'
+	'--------------------'
+)		
 		
 
-#write to file
+#-------------------
+#Return results to file
 with open(analysis_txt, 'w', newline='') as output:
-	output.write('Election Results\n')
-	output.write('--------------------\n')
-	output.write(f'Total Votes: {total_votes}\n')
-	output.write('--------------------\n')
+	output.write(
+		'Election Results\n'
+		'--------------------\n'
+		f'Total Votes: {sum(votes)}\n'
+		'--------------------\n'
+	)
 	for x in range(0,len(candidates)):
-		output.write(f'{candidates[x]}: {round(votes[x] / total_votes * 100,2)}% ({votes[x]})\n')
-	output.write('--------------------\n')
-	output.write(f'Winner: {candidates[winner_index]}\n')
-	output.write('--------------------\n')	
+		output.write(f'{candidates[x]}: {round(votes[x] / sum(votes) * 100,2)}% ({votes[x]})\n')
+	output.write(
+		'--------------------\n'
+		f'Winner: {candidates[winner_index]}\n'
+		'--------------------\n'
+	)	
 
 
 
